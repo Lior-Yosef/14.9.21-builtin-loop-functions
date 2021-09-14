d23a7fb4e16bc15d6138e897e3c20a952ae21f39
Lior-Yosef <lioryosef06@gmail.com>
2021-09-14 22:36:18 +0300

    first comment

---
+++ main.js
@@ -0,0 +1,150 @@
+// let ArticlesArray = [
+//     {
+//         headline: "new hotel in Tel-Aviv",
+//         author: "lior",
+//         date: 2021
+//     },
+//     {
+//         headline: "new hotel in haifa",
+//         author: "israel",
+//         date: 2020
+//     },
+//     {
+//         headline: "new hotel in eilat",
+//         author: "yosi",
+//         date: 2019
+//     }
+// ];
+
+///////////////// question number 1 ////////////////////
+
+// ArticlesArray.forEach((headline,index)=>{
+//     console.log(headline);
+// })
+
+// for (const obj of ArticlesArray){
+//     console.log(obj);
+// }
+
+///////////////////////////////////////////////////////
+
+
+
+///////////////// question number 2 ////////////////////
+
+//  let cars = [
+//      {
+//          price: 180000,
+//          year: 2021,
+//          model: "tesla",
+//      },
+
+//      {
+//          price: 250000,
+//          year: 2022,
+//          model: "Mercedes-Benz",
+
+//      },
+
+//      {
+//          price: 120000,
+//          year: 2021,
+//          model: "Porsche",
+
+//      },
+
+//      {
+//          price: 120000,
+//          year: 2017,
+//          model: "Mercedes-Amg",
+
+//      },
+
+//      {
+//          price: 190000,
+//          year: 2020,
+//          model: "Mercedes-Maybach",
+//      }
+//  ];
+
+
+//  for (let i = 0; i < cars.length; i++) {
+
+//     for(const key in cars[i]){
+//         console.log(`${key} ${cars[i][key]}`);
+        
+//     } 
+     
+//  }
+
+
+
+
+///////////////////////////////////////////////////////
+
+
+///////////////// question number 3 ////////////////////
+
+
+
+//  let BooksArray = [
+//      {
+//          headline: "Tel-Aviv",
+//          author: "lior",
+//          price: 200
+//      },
+//      {
+//          headline: " haifa",
+//          author: "israel",
+//          price: 300
+//      },
+//      {
+//          headline: "eilat",
+//          author: "yosi",
+//          price: 400
+//      }
+//  ];
+
+//  BooksArray.forEach((headline,index)=>{
+//      console.log(headline);
+//  })
+
+//  BooksArray.forEach((headline,index)=>{
+//     console.log(`name of author: ${headline.author}`);
+// })
+
+///////////////////////////////////////////////////////
+
+
+///////////////// question number 4 ////////////////////
+
+// let numbers=[6,4,8,1,5];
+
+// // let sum=0;
+
+// //     numbers.forEach((num, index)=>{
+// //         console.log(sum+=num);
+// //     })
+    
+//      let result = 0;
+//  for (const resultNumber of numbers) {
+//      result += resultNumber;
+//  }
+//  console.log(result);
+
+
+///////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
